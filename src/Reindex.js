5f081ecfd7ec2ec6a843addd08efb6923da6880c
fakeAssBrian <brian+test@netlify.com>
2016-12-16 12:08:04 -0800

    Update Reindex.js

--- src/Reindex.js
+++ src/Reindex.js
@@ -1,6 +1,8 @@
 import Reindex from 'reindex-js';
 import Config from './config';
 
+localStorage.setItem('REINDEX_TOKEN', Config.REINDEX_ID);
+
 const reindex = new Reindex(Config.REINDEX_URL);
 
 export default reindex;
