35a53c98e59445a3c1fcbe790974e7f7854d29c9
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2016-12-16 12:19:40 -0800

    add uri

--- src/Reindex.js
+++ src/Reindex.js
@@ -1,8 +1,16 @@
 import Reindex from 'reindex-js';
-import Config from './config';
+// import Config from './config';
 
-localStorage.setItem('REINDEX_TOKEN', Config.REINDEX_ID);
+import URI from 'urijs';
 
-const reindex = new Reindex(Config.REINDEX_URL);
+localStorage.setItem('REINDEX_TOKEN', process.env.REINDEX_ID);
+
+function ensureAbsoluteUrl(path) {
+  if (typeof path !== 'string') return path;
+  if (URI(path).is('absolute')) return path;
+  return URI(path).normalize().toString();
+}
+
+const reindex = new Reindex(ensureAbsoluteUrl(process.env.REINDEX_URL));
 
 export default reindex;
