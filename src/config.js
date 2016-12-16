f753abc1a3f4b2aa438c0a9e1e5b8fd9d3c275f2
fakeAssBrian <brian+test@netlify.com>
2016-12-16 08:41:48 -0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  REINDEX_URL: process.env.REINDEX_URL,
-  REINDEX_ID: process.env.REINDEX_ID,
+  REINDEX_URL: process.env.REINDEX_URL || "https://oxidized-cerium-209.myreindex.com",
+  REINDEX_ID: process.env.REINDEX_ID || "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc0FkbWluIjp0cnVlLCJpYXQiOjE0NjI4Mjg0NDZ9.ESiqn5PlJNPagQFj4OxzMC7BV0iCOPqvir0anjUX8NY",
 };
